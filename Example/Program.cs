47a42f34112ea450549812f41025415e8413d145
pabir@mail.ru <pabir@mail.ru@591ae5d2-a641-0410-9af0-57f1f6311122>
2007-12-21 17:18:25 +0000

    Developing

---
+++ Example/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+
+using NConsoler;
+
+namespace NConsoler.Example
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Consoler.Run(typeof(Program), args);
+		}
+
+		// consoler.exe 10 "description" /b
+		[Action]
+		public static void Delete(
+			[Required(Description = "Object count")]
+			int count,
+
+			[Required(Description = "Object description")]
+			string description,
+
+			[Optional(false, "b", "bk", Description = "Boolean value")]
+			bool book,
+
+			[Optional("")]
+			string comment)
+		{
+			Console.WriteLine("Delete {0} {1} {2} {3}", count, description, book, comment);
+		}
+	}
+}
