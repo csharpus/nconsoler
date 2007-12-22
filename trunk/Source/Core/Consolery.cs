b024b0b48a4bb8b12bd63567662a789dabd14c59
pabir@mail.ru <pabir@mail.ru@591ae5d2-a641-0410-9af0-57f1f6311122>
2007-12-22 13:00:14 +0000

    unit tests

--- trunk/Source/Core/Consolery.cs
+++ trunk/Source/Core/Consolery.cs
@@ -53,6 +53,23 @@ namespace NConsoler
 			return attributes.Length == 0 || attributes[0].GetType() == typeof(RequiredAttribute);
 		}
 
+		private bool IsOptional(ParameterInfo info)
+		{
+			return !IsRequired(info);
+		}
+
+		private OptionalAttribute GetOptional(ParameterInfo info)
+		{
+			object[] attributes = info.GetCustomAttributes(typeof(OptionalAttribute), false);
+			return attributes[0] as OptionalAttribute;
+		}
+
+		private RequiredAttribute GetRequired(ParameterInfo info)
+		{
+			object[] attributes = info.GetCustomAttributes(typeof(RequiredAttribute), false);
+			return attributes[0] as RequiredAttribute;
+		}
+
 		object ConvertValue(string value, Type argumentType)
 		{
 			if (argumentType == typeof(int))
@@ -61,11 +78,11 @@ namespace NConsoler
 				{
 					return Convert.ToInt32(value);
 				}
-				catch (FormatException fe)
+				catch (FormatException)
 				{
 					throw new NConsolerException("Could not convert \"{0}\" to integer", value);
 				}
-				catch (OverflowException oe)
+				catch (OverflowException)
 				{
 					throw new NConsolerException("Value \"{0}\" is too big or too small", value);
 				}
@@ -104,7 +121,8 @@ namespace NConsoler
 
 		private void Run()
 		{
-			Validate();
+			ValidateMetadata();
+			ValidateInput();
 			if (_actionMethods.Count == 1)
 			{
 				int argumentIndex = 0;
@@ -117,25 +135,19 @@ namespace NConsoler
 					if (IsRequired(info))
 					{
 						requiredParameterCount++;
-						if (_args.Length < requiredParameterCount)
-						{
-							throw new NConsolerException("Not all required parameters is set");
-						}
 						parameterValues.Add(ConvertValue(_args[argumentIndex], info.ParameterType));
-						
 					}
 					else // /a /a:value /-a
 					{
-						
 						object[] attributes = info.GetCustomAttributes(typeof(ParameterAttribute), false);
 						OptionalAttribute optional = attributes[0] as OptionalAttribute;
-						
+
 						foreach (string altName in optional.AltNames)
 						{
-							aliases.Add(altName, 
+							aliases.Add(altName,
 								new ParameterData(parameterValues.Count, info.ParameterType, info.Name));
 						}
-						aliases.Add(info.Name, 
+						aliases.Add(info.Name,
 							new ParameterData(parameterValues.Count, info.ParameterType, info.Name));
 						parameterValues.Add(optional.Default);
 					}
@@ -144,10 +156,10 @@ namespace NConsoler
 				Dictionary<string, string> values = new Dictionary<string, string>();
 				for (int i = requiredParameterCount; i < _args.Length; i++)
 				{
-					if (!_args[i].StartsWith("/"))
-					{
-						throw new NConsolerException("Unknown parameter {0}", _args[i]);
-					}
+					//if (!_args[i].StartsWith("/"))
+					//{
+					//    throw new NConsolerException("Unknown parameter {0}", _args[i]);
+					//}
 					if (_args[i].Contains(":"))
 					{
 						int semicolonPosition = _args[i].IndexOf(':');
@@ -189,61 +201,173 @@ namespace NConsoler
 			}
 		}
 
-		private void Validate()
+		private int RequiredParameterCount(MethodInfo method)
+		{
+			int requiredParameterCount = 0;
+			foreach (ParameterInfo parameter in method.GetParameters())
+			{
+				if (IsRequired(parameter))
+				{
+					requiredParameterCount++;
+				}
+			}
+			return requiredParameterCount;
+		}
+
+		#region Validation
+
+		private void ValidateInput()
+		{
+			if (_actionMethods.Count == 1)
+			{
+				CheckAllRequiredParametersAreSet(_actionMethods[0]);
+				CheckOptionalParametersAreNotDuplicated(_actionMethods[0]);
+			}
+			else
+			{
+			}
+		}
+
+		private void CheckAllRequiredParametersAreSet(MethodInfo method)
+		{
+			if (_args.Length < RequiredParameterCount(method))
+			{
+				throw new NConsolerException("Not all required parameters are set");
+			}
+		}
+
+		private string ParameterName(string parameter)
+		{
+			if (parameter.StartsWith("/-"))
+			{
+				return parameter.Substring(2);
+			}
+			else if (parameter.Contains(":"))
+			{
+				return parameter.Substring(1, parameter.IndexOf(":") - 1);
+			}
+			else
+			{
+				return parameter.Substring(1);
+			}
+		}
+
+		private void CheckOptionalParametersAreNotDuplicated(MethodInfo method)
+		{
+			List<string> passedParameters = new List<string>();
+			for (int i = RequiredParameterCount(method); i < _args.Length; i++)
+			{
+				if (!_args[i].StartsWith("/"))
+				{
+					throw new NConsolerException("Unknown parameter {0}", _args[i]);
+				}
+				string name = ParameterName(_args[i]);
+				if (passedParameters.Contains(name))
+				{
+					throw new NConsolerException("Parameter with name {0} passed two times", name);
+				}
+				passedParameters.Add(name);
+			}
+		}
+
+		private void CheckUnknownParametersAreNotPassed(MethodInfo method)
+		{
+		}
+
+		private void ValidateMetadata()
+		{
+			CheckAnyActionMethodExists();
+			foreach (MethodInfo method in _actionMethods)
+			{
+				CheckRequiredAndOptionalAreNotAppliedAtTheSameTime(method);
+				CheckOptionalParametersAreAfterRequiredOnes(method);
+				CheckOptionalParametersDefaultValuesAreAssignableToRealParameterTypes(method);
+				CheckOptionalParametersAltNamesAreNotDuplicated(method);
+			}
+		}
+
+		private void CheckAnyActionMethodExists()
 		{
 			if (_actionMethods.Count == 0)
 			{
-				throw new NConsolerException("Can not find any public static method in type \"{0}\" marked with [Action] attribute ", _targetType.Name);
+				throw new NConsolerException("Can not find any public static method in type \"{0}\" marked with [Action] attribute", _targetType.Name);
 			}
-			int requiredParameterCount = 0;
-			foreach (MethodInfo methodInfo in _actionMethods)
+		}
+
+		private void CheckRequiredAndOptionalAreNotAppliedAtTheSameTime(MethodInfo method)
+		{
+			foreach (ParameterInfo parameter in method.GetParameters())
 			{
-				List<string> parameterNames = new List<string>();
-				bool optionalFound = false;
-				foreach (ParameterInfo info in methodInfo.GetParameters())
+				object[] attributes = parameter.GetCustomAttributes(typeof(ParameterAttribute), false);
+				if (attributes.Length > 1)
 				{
-					object[] attributes = info.GetCustomAttributes(typeof(ParameterAttribute), false);
-					if (attributes.Length > 1)
+					throw new NConsolerException("More than one attribute is applied to parameter \"{0}\" in method \"{1}\"", parameter.Name, method.Name);
+				}
+			}
+		}
+
+		private void CheckOptionalParametersDefaultValuesAreAssignableToRealParameterTypes(MethodInfo method)
+		{
+			foreach (ParameterInfo parameter in method.GetParameters())
+			{
+				if (IsRequired(parameter))
+				{
+					continue;
+				}
+				OptionalAttribute optional = GetOptional(parameter);
+				if (!optional.Default.GetType().IsAssignableFrom(parameter.ParameterType))
+				{
+					throw new NConsolerException("Default value for an optional parameter \"{0}\" in method \"{1}\" can not be assigned to the parameter", parameter.Name, method.Name);
+				}
+			}
+		}
+
+		private void CheckOptionalParametersAreAfterRequiredOnes(MethodInfo method)
+		{
+			bool optionalFound = false;
+			foreach (ParameterInfo parameter in method.GetParameters())
+			{
+				if (IsOptional(parameter))
+				{
+					optionalFound = true;
+				}
+				else if (optionalFound)
+				{
+					throw new NConsolerException("It is not allowed to write a parameter with a Required attribute after a parameter with an Optional one. See method \"{0}\" parameter \"{1}\"", method.Name, parameter.Name);
+				}
+			}
+		}
+
+		private void CheckOptionalParametersAltNamesAreNotDuplicated(MethodInfo method)
+		{
+			List<string> parameterNames = new List<string>();
+			foreach (ParameterInfo parameter in method.GetParameters())
+			{
+				if (IsRequired(parameter))
+				{
+					parameterNames.Add(parameter.Name);
+				}
+				else
+				{
+					if (parameterNames.Contains(parameter.Name))
 					{
-						throw new NConsolerException("More than one attribute is applied to parameter \"{0}\" in method \"{1}\"", info.Name, methodInfo.Name);
+						throw new NConsolerException("Found duplicated parameter name \"{0}\" in method \"{1}\". Please check alt names for optional parameters", parameter.Name, method.Name);
 					}
-					if (attributes.Length == 1 && attributes[0].GetType() == typeof(OptionalAttribute))
+					parameterNames.Add(parameter.Name);
+					OptionalAttribute optional = GetOptional(parameter);
+					foreach (string altName in optional.AltNames)
 					{
-						optionalFound = true;
-						OptionalAttribute optional = (attributes[0] as OptionalAttribute);
-						if (!optional.Default.GetType().IsAssignableFrom(info.ParameterType))
+						if (parameterNames.Contains(altName))
 						{
-							throw new NConsolerException("Default value for an optional parameter \"{0}\" in method \"{1}\" can not be assigned to the parameter", info.Name, methodInfo.Name);
+							throw new NConsolerException("Found duplicated parameter name \"{0}\" in method \"{1}\". Please check alt names for optional parameters", altName, method.Name);
 						}
-					}
-					if (IsRequired(info))
-					{
-						requiredParameterCount++;
-						parameterNames.Add(info.Name);
-					}
-					else
-					{
-						if (parameterNames.Contains(info.Name))
-						{
-							throw new NConsolerException("Found duplicated parameter name \"{0}\" in method \"{1}\". Please check alt names for optional parameters", info.Name, methodInfo.Name);
-						}
-						parameterNames.Add(info.Name);
-						OptionalAttribute optional = attributes[0] as OptionalAttribute;
-						foreach (string altName in optional.AltNames)
-						{
-							if (parameterNames.Contains(altName))
-							{
-								throw new NConsolerException("Found duplicated parameter name \"{0}\" in method \"{1}\". Please check alt names for optional parameters", altName, methodInfo.Name);
-							}
-						}
-					}
-					if (optionalFound && IsRequired(info))
-					{
-						throw new NConsolerException("It is not allowed to write a parameter with a Required attribute after a parameter with an Optional one. See method \"{0}\" parameter \"{1}\"", methodInfo.Name, info.Name);
+						parameterNames.Add(altName);
 					}
 				}
 			}
 		}
+
+		#endregion
 	}
 
 	public interface IMessenger
@@ -259,10 +383,12 @@ namespace NConsoler
 		}
 	}
 
+	[AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
 	public sealed class ActionAttribute : Attribute
 	{
 	}
 
+	[AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false)]
 	public class ParameterAttribute : Attribute
 	{
 		private string _description;
@@ -315,7 +441,7 @@ namespace NConsoler
 			}
 		}
 
-		public OptionalAttribute(object defaultValue, params string[] altNames) 
+		public OptionalAttribute(object defaultValue, params string[] altNames)
 			: base(false)
 		{
 			_defaultValue = defaultValue;
@@ -328,7 +454,7 @@ namespace NConsoler
 		public RequiredAttribute()
 			: base(true)
 		{
-			
+
 		}
 	}
 
