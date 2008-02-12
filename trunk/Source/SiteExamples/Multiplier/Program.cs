2dd32c53f1129d7e6bb14b9e2011d58868530c38
pabir@mail.ru <pabir@mail.ru@591ae5d2-a641-0410-9af0-57f1f6311122>
2008-02-12 22:06:13 +0000

    Added Site examples

---
+++ trunk/Source/SiteExamples/Multiplier/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NConsoler;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		Consolery.Run(typeof(Program), args);
+	}
+
+[Action]
+public static void Multiple(
+	[Required(Description = "1st multiplier")]
+		int factor1,
+
+	[Required(Description = "2nd multiplier")]
+		int factor2,
+
+	[Optional(true, Description = "Show program logo")]
+		bool showlogo)
+{
+	if (showlogo)
+	{
+		Console.WriteLine("Multiplier example");
+	}
+	Console.WriteLine(factor1 * factor2);
+}
+}
---
+++ trunk/Source/Tests/SiteExamples.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace NConsoler.Tests
+{
+	[TestFixture]
+	public class SiteExamples
+	{
+		MockRepository mocks;
+		static IMessenger messenger;
+
+		[SetUp]
+		public void Setup()
+		{
+			mocks = new MockRepository();
+			messenger = mocks.CreateMock<IMessenger>();
+		}
+
+		[Test]
+		public void HelloWorldExampleFromQuickStart()
+		{
+			HelloWorldExample.Main(new string[] { "Hello World" });
+		}
+
+		class HelloWorldExample
+		{
+			public static void Main(string[] args)
+			{
+				Consolery.Run(typeof(HelloWorldExample), args);
+			}
+
+			[Action]
+			public static void ShowMessage(string message)
+			{
+				Console.WriteLine(message);
+			}
+		}
+	}
+}
