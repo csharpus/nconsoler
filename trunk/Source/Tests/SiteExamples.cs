d3facdf1bafd1a3a897b003a9aa2e00718f69f80
pabir@mail.ru <pabir@mail.ru@591ae5d2-a641-0410-9af0-57f1f6311122>
2008-07-24 07:49:33 +0000

    Added site examples

--- trunk/Source/Tests/SiteExamples.cs
+++ trunk/Source/Tests/SiteExamples.cs
@@ -1,8 +1,7 @@
-﻿namespace NConsoler.Tests
+﻿// <examples>
+namespace NConsoler.Tests
 {
     using System;
-    using System.Collections.Generic;
-    using System.Text;
     using NUnit.Framework;
     using Rhino.Mocks;
 
@@ -56,6 +55,7 @@
 
         class Parameters
         {
+// <example name="parameters">
             [Action]
             public static void Method(
                 [Required]
@@ -70,8 +70,143 @@
                 DateTime p5,
                 [Optional(false)]
                 bool flag)
+// </example>
             {
             }
         }
+
+        [Test]
+        public void CheatSheetExamples()
+        {
+			using (mocks.Record())
+			{
+			}
+			using (mocks.Playback())
+			{
+				Consolery.Run(
+					typeof(CheatSheet),
+					new string[] { "first", "/optional:2", @"/value:3" },
+					messenger);
+			} 
+        }
+
+        class CheatSheet {
+// <example name="action_method_cheath_sheet">
+			[Action]
+			public static void Method(
+// </example>
+// <example name="required_parameter_cheath_sheet">
+				[Required(Description = "Parameter name")]
+				string name,
+// </example>
+// <example name="optional_parameter_cheath_sheet">
+				[Optional("Parameter name")]
+				string optional,
+// </example>
+// <example name="alias_cheath_sheet">
+				[Optional("Default value", "anAlias")]
+				string value
+// </example>
+			)
+			{
+			}
+		}
+
+		[Test]
+		public void MultipleActionsExample()
+		{
+			using (mocks.Record())
+			{
+			}
+			using (mocks.Playback())
+			{
+				Consolery.Run(
+					typeof(MultipleExamples),
+					new string[] { "add", "Bender" },
+					messenger);
+			} 
+		}
+
+		class MultipleExamples
+		{
+// <example name="multiple_actions">
+			[Action]
+			public static void Add(
+				[Required]
+				string userName)
+			{
+
+			}
+
+			[Action]
+			public static void Remove(
+				[Required]
+				string userName)
+			{
+
+			}
+// </example>
+		}
+
+		[Test]
+		public void ValidationExample()
+		{
+			using (mocks.Record())
+			{
+				messenger.Write("It is not allowed to write a parameter with a Required attribute after a parameter with an Optional one. See method \"Method\" parameter \"required\"");
+			}
+			using (mocks.Playback())
+			{
+				Consolery.Run(
+					typeof(Validation),
+					new string[] { "add", "Bender" },
+					messenger);
+			} 
+		}
+
+		class Validation
+		{
+// <example name="validation">
+			[Action]
+			public static void Method(
+				[Optional("default")]
+				string optional,
+				[Required]
+				string required)
+// </example>
+			{
+				
+			}
+		}
+
+		[Test]
+		public void HelpExample()
+		{
+			using (mocks.Record())
+			{
+			}
+			using (mocks.Playback())
+			{
+				Consolery.Run(
+					typeof(Help),
+					new string[] { },
+					messenger);
+			} 
+		}
+
+		class Help
+		{
+// <example name="help">
+			[Action]
+			public static void Method(
+				[Required(Description = "Applies some magic")]
+				string flag)
+// </example>
+			{
+				
+			}
+
+		}
 	}
 }
+// </examples>
