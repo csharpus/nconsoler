524ca9a6ebd664651baa572bcdf01818e81a4842
Michal Altair Valášek <github@rider.cz>
2019-05-26 17:36:40 +0200

    Created .NET Standard project

---
+++ NConsoler/Extensions/TypeExtension.cs
@@ -0,0 +1,14 @@
+﻿namespace NConsoler.Extensions
+{
+	using System;
+
+	public static class TypeExtension
+	{
+		public static bool CanBeNull(this Type type)
+		{
+			return type == typeof(string)
+				   || type == typeof(string[])
+				   || type == typeof(int[]);
+		}
+	}
+}
---
+++ NConsoler/IMessenger.cs
@@ -0,0 +1,10 @@
+﻿namespace NConsoler
+{
+	/// <summary>
+	/// Used for getting messages from NConsoler
+	/// </summary>
+	public interface IMessenger
+	{
+		void Write(string message);
+	}
+}
